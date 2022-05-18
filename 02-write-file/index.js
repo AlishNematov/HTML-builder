b754949e1f3947f966576474902b53a1f4e615ab
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-05-18 22:29:28 +0500

    feat: solve 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout, stdin, exit } = process;
+
+stdout.write('Введите сообщение для записи:\n')
+
+fs.appendFile(path.resolve(__dirname, 'text.txt'), '', (err) => {
+    if(err){
+        throw err;
+    }
+});
+
+stdin.on('data', data => {
+    if(data.toString().slice(0, 4) === 'exit') {
+        exit();
+    }
+    fs.appendFile(path.resolve(__dirname, 'text.txt'), data, (err) => {
+        if(err){
+            throw err;
+        }
+    });
+});
+
+process.on('SIGINT', () => {
+    exit();
+});
+
+process.on('exit', () => {
+    console.log("Удачи!");
+    exit();
+});
+
+
