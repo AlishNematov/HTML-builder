40fd1c02190f0ed6045f824f20f81ed35fcace91
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-05-19 14:22:27 +0500

    refactor: apply eslint 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
 const path  = require('path');
-const { stdout, stdin, exit } = process;
+const { stdout } = process;
 
 fs.readdir(path.resolve(__dirname, 'secret-folder'), {withFileTypes: true}, (err, items) => {
-    for (var i=0; i<items.length; i++) {
-        let information = [];
-        if(items[i].isFile()) {
-            information.push(items[i].name.slice(0, items[i].name.lastIndexOf('.')));
-            information.push(path.parse(items[i].name).ext.slice(1));
-            fs.stat(path.resolve(__dirname, 'secret-folder', items[i].name), (err, stats) => {
-                if(err) {throw err}
-                information.push(`${stats.size/1000}kb`);
-                stdout.write(`${information.join(' - ')}\n`)
-            });
-        }
+  if (err) {
+    throw err;
+  }
+  for (const item of items) {
+    let information = [];
+    if(item.isFile()) {
+      information.push(item.name.slice(0, item.name.lastIndexOf('.')));
+      information.push(path.parse(item.name).ext.slice(1));
+      fs.stat(path.resolve(__dirname, 'secret-folder', item.name), (err, stats) => {
+        if(err) {throw err;}
+        information.push(`${stats.size/1000}kb`);
+        stdout.write(`${information.join(' - ')}\n`);
+      });
     }
-})
+  }
+});
