92704300c16af3106a8ff151a980bb2789280924
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-05-22 11:29:50 +0500

    fix: change solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,41 +1,49 @@
 const fs = require('fs');
 const path  = require('path');
 
-function copyDir() {
+function copyDir(sourcePath, destinatonPath) {
 
-  fs.readdir(path.resolve(__dirname, 'files-copy'), (err, items) => {
+  fs.readdir(destinatonPath, async (err, items) => {
     if(err) {
-      fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, err => {
-        if (err) {
-          throw err;
-        }
+      await fs.mkdir(destinatonPath, { recursive: true }, err => {
+        if (err) throw err;
       });
     }
     if (items) {
       for(const item of items) {
-        fs.unlink(path.join(__dirname, 'files-copy', item), err => {
-          if (err) throw err;
-        });
+        await fs.stat(path.resolve(destinatonPath, item), async (err, stats) => {
+          if(err) throw err;
+          if(stats.isFile()) {
+            await fs.unlink(path.join(destinatonPath, item), err => {
+              if (err) throw err;
+            });
+          }
+        })
       }
     }
   });
 
-  fs.readdir(path.resolve(__dirname, 'files'), (err, items) => {
+  fs.readdir(sourcePath, (err, items) => {
     if(err) {
       throw err;
     }
     for (const item of items) {
-      fs.copyFile(path.resolve(__dirname, 'files', item), path.resolve(__dirname, 'files-copy', item), err=> {
-        if(err) {
-          fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, err => {
-            if (err) {
-              throw err;
+      fs.stat(path.resolve(sourcePath, item), (err, stats) => { 
+        if(err) throw err;
+        if(stats.isFile()) {
+          fs.copyFile(path.resolve(sourcePath, item), path.resolve(destinatonPath, item), err=> {
+            if(err) {
+              fs.mkdir(destinatonPath, { recursive: true }, err => {
+                if (err) throw err;
+              });
             }
           });
+        } else {
+          copyDir(path.resolve(sourcePath, item), path.resolve(destinatonPath, item));
         }
-      });
+      })
     }
   });
 }
 
-copyDir();
+copyDir(path.resolve(__dirname, 'files'), path.resolve(__dirname, 'files-copy'));
